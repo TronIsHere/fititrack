56efd4a1e2fcd6a8a73851197aabc7dbcf899703
Erwin <erwin0maleki@gmail.com>
2023-09-05 18:12:57 -0700

    update time

--- src/pages/waitlist/joined.tsx
+++ src/pages/waitlist/joined.tsx
@@ -29,7 +29,7 @@ const JoinedPage: NextPage = () => {
         });
         setTimeout(() => {
           router.push("/waitlist");
-        }, 2000);
+        }, 1000);
       }
     };
     getWaitlistNumbers();
