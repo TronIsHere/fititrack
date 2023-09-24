ff5f37253c50af1475278c038708f32cadb3369f
Erwin <erwin0maleki@gmail.com>
2023-09-24 12:19:22 -0700

    half way through workout page

--- src/pages/dashboard/workouts/index.tsx
+++ src/pages/dashboard/workouts/index.tsx
@@ -1,8 +1,26 @@
+import WorkoutComponent from "@/components/dashboard/workout";
 import DashboardLayout from "@/components/layouts/dashboardLayout";
 import { MyPage } from "@/components/types/nextjs";
 
 const WorkoutsPage: MyPage = () => {
-  return <>workouts</>;
+  return (
+    <>
+      <div className="grid grid-cols-7 gap-6">
+        <div className="col-span-5">
+          <div className="flex justify-between items-center mb-10">
+            <span className="mt-4 block text-2xl font-bold">Workouts</span>
+            <button className="bg-palletPurple-500 h-10 text-white px-8 rounded-md mt-3">
+              Add
+            </button>
+          </div>
+          <WorkoutComponent />
+          <WorkoutComponent />
+          <WorkoutComponent />
+        </div>
+        <div className="col-span-2"></div>
+      </div>
+    </>
+  );
 };
 WorkoutsPage.getLayout = (page: any) => {
   return <DashboardLayout>{page}</DashboardLayout>;
