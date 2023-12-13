98b9a014f95555ad2610f6dfb78b5aaabf32f0c2
Erwin <erwin0maleki@gmail.com>
2023-12-12 16:57:28 -0800

    fixed some UI

--- src/components/dashboard/weight.tsx
+++ src/components/dashboard/weight.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GiWeightScale } from "react-icons/gi";
+import { FaPlusCircle } from "react-icons/fa";
 import { MyPage } from "@/components/types/nextjs";
 import { Button } from "@/components/ui/button";
 import {
@@ -17,10 +18,18 @@ const WeightComponent = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <div className="bg-white rounded-xl p-5 w-full md:w-56">
-          <div className="flex">
-            <GiWeightScale color="#5955ED" size={28} />
-            <span className="pl-5 pt-1">Weight</span>
+        <div className="group bg-white rounded-xl p-5 w-full md:w-56 cursor-pointer">
+          <div className="flex justify-between  ">
+            <div className="flex">
+              {" "}
+              <GiWeightScale color="#5955ED" size={28} />
+              <span className="pl-5 pt-1">Weight</span>
+            </div>
+            <FaPlusCircle
+              color="#5955ED"
+              size={28}
+              className="-mt-2  -mr-2 opacity-20 hidden group-hover:block"
+            />
           </div>
           <div className="flex justify-center pt-8">
             <span className="text-3xl">
