59393a7eec3251747d3b0370b74f2e5210599de9
Erwin <erwin0maleki@gmail.com>
2024-01-12 01:11:04 -0800

    refactored nav

---
+++ src/components/ui/navbar/navlink.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+interface NavLinkProps {
+  href: string;
+  icon: JSX.Element;
+  label: string;
+}
+
+// Reusable component for navigation links
+const NavLink = ({ href, icon, label }: NavLinkProps) => (
+  <Link
+    href={href}
+    className="flex items-center text-palletGray-200 py-2 px-6 rounded-full cursor-pointer hover:text-palletPurple-500 hover:bg-palletPurple-200 duration-500"
+  >
+    {icon}
+    <span className="pl-3">{label}</span>
+  </Link>
+);
+
+export default NavLink;
--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,3 +1,5 @@
+// path/filename: /components/SidebarComponent.tsx
+
 import { NextPage } from "next";
 import Link from "next/link";
 import { useState } from "react";
@@ -6,68 +8,69 @@ import { FaChartLine } from "react-icons/fa6";
 import { FiActivity, FiLogOut } from "react-icons/fi";
 import { HiMenuAlt3 } from "react-icons/hi";
 import { IoSettings } from "react-icons/io5";
-interface sidebarProps {
-  darkMode: Boolean;
+import NavLink from "./navbar/navlink";
+
+interface SidebarProps {
+  darkMode: boolean;
 }
-const SidebarComponent: NextPage<sidebarProps> = ({ darkMode }) => {
-  const [mobileMenu, setMobileMenu] = useState<boolean>(false);
+
+const SidebarComponent: NextPage<SidebarProps> = ({ darkMode }) => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  const links = [
+    {
+      href: "/dashboard",
+      icon: <BiSolidDashboard size={20} />,
+      label: "Dashboard",
+    },
+    {
+      href: "/dashboard/workouts",
+      icon: <FiActivity size={20} />,
+      label: "Workouts",
+    },
+    {
+      href: "/dashboard/history",
+      icon: <FaChartLine size={20} />,
+      label: "History",
+    },
+    {
+      href: "/dashboard/settings",
+      icon: <IoSettings size={20} />,
+      label: "Settings",
+    },
+    { href: "/", icon: <FiLogOut size={20} />, label: "Logout" },
+  ];
+
   return (
     <>
-      <div className="hidden md:flex flex-col   ">
+      <div className="hidden md:flex flex-col">
         <Link href={"/dashboard"} className="self-center">
           <img
             src="/images/logo.svg"
             alt=""
             width={180}
-            className=" mt-8  bg-white p-3 rounded-xl"
+            className="mt-8 bg-white p-3 rounded-xl"
           />
         </Link>
         <ul className="mt-14 m-8">
-          <Link
-            href={"/dashboard"}
-            className="flex items-center text-palletGray-200 py-2 px-6 rounded-full cursor-pointer hover:text-palletPurple-500 hover:bg-palletPurple-200 duration-500"
-          >
-            <BiSolidDashboard size={20} />
-            <span className=" pl-3">Dashboard</span>
-          </Link>
-          <Link
-            href={"/dashboard/workouts"}
-            className="flex items-center text-palletGray-200 py-2 px-6 rounded-full cursor-pointer mt-3 hover:text-palletPurple-500 hover:bg-palletPurple-200 duration-500"
-          >
-            <FiActivity className="" size={20} />
-            <span className=" pl-3">Workouts</span>
-          </Link>
-          <Link
-            href={"/dashboard/history"}
-            className="flex items-center text-palletGray-200 py-2 px-6 rounded-full cursor-pointer mt-3 hover:text-palletPurple-500 hover:bg-palletPurple-200 duration-500"
-          >
-            <FaChartLine className="" size={20} />
-            <span className=" pl-3">History</span>
-          </Link>
-          <Link
-            href={"/dashboard/settings"}
-            className="flex items-center text-palletGray-200 py-2 px-6 rounded-full cursor-pointer mt-3 hover:text-palletPurple-500 hover:bg-palletPurple-200 duration-500"
-          >
-            <IoSettings size={20} />
-            <span className=" pl-3">Settings</span>
-          </Link>
-          <Link
-            href={"/"}
-            className="flex items-center text-palletGray-200 py-2 px-6 rounded-full cursor-pointer mt-3 hover:text-palletPurple-500 hover:bg-palletPurple-200 transition-colors duration-500"
-          >
-            <FiLogOut className="" size={20} />
-            <span className=" pl-3">Logout</span>
-          </Link>
+          {links.map((link, index) => (
+            <NavLink
+              key={index}
+              href={link.href}
+              icon={link.icon}
+              label={link.label}
+            />
+          ))}
         </ul>
       </div>
-      <div className="flex md:hidden flex-col  ">
-        <div className="flex justify-around items-center  pb-4">
+      <div className="flex md:hidden flex-col">
+        <div className="flex justify-around items-center pb-4">
           <Link href={"/dashboard"}>
             <img
               src="/images/logo.svg"
               alt=""
               width={180}
-              className="mt-8 bg-white  rounded-lg p-3"
+              className="mt-8 bg-white rounded-lg p-3"
             />
           </Link>
           <HiMenuAlt3
@@ -77,42 +80,21 @@ const SidebarComponent: NextPage<sidebarProps> = ({ darkMode }) => {
             onClick={() => setMobileMenu(!mobileMenu)}
           />
         </div>
-        {mobileMenu ? (
+        {mobileMenu && (
           <ul className="mt-5 my-8 mx-4">
-            <Link
-              href={"/dashboard"}
-              className="flex items-center text-palletGray-200 py-2 px-6 rounded-full cursor-pointer hover:text-palletPurple-500 hover:bg-palletPurple-200 duration-500"
-            >
-              <BiSolidDashboard size={20} />
-              <span className=" pl-3">Dashboard</span>
-            </Link>
-            <Link
-              href={"/dashboard/workouts"}
-              className="flex items-center text-palletGray-200 py-2 px-6 rounded-full cursor-pointer mt-3 hover:text-palletPurple-500 hover:bg-palletPurple-200 duration-500"
-            >
-              <FiActivity className="" size={20} />
-              <span className=" pl-3">Workouts</span>
-            </Link>
-            <Link
-              href={"/dashboard/settings"}
-              className="flex items-center text-palletGray-200 py-2 px-6 rounded-full cursor-pointer mt-3 hover:text-palletPurple-500 hover:bg-palletPurple-200 duration-500"
-            >
-              <IoSettings size={20} />
-              <span className=" pl-3">Settings</span>
-            </Link>
-            <Link
-              href={"/"}
-              className="flex items-center text-palletGray-200 py-2 px-6 rounded-full cursor-pointer mt-3 hover:text-palletPurple-500 hover:bg-palletPurple-200 transition-colors duration-500"
-            >
-              <FiLogOut className="" size={20} />
-              <span className=" pl-3">Logout</span>
-            </Link>
+            {links.map((link, index) => (
+              <NavLink
+                key={index}
+                href={link.href}
+                icon={link.icon}
+                label={link.label}
+              />
+            ))}
           </ul>
-        ) : (
-          ""
         )}
       </div>
     </>
   );
 };
+
 export default SidebarComponent;
